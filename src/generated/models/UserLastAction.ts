774607def32dffb2963bf314b54d73dd052d1f48
Neon0404 <neon040404@gmail.com>
2025-10-28 08:18:42 +0000

    chore(api): regenerate client for API v4.2.4

--- src/generated/models/UserLastAction.ts
+++ src/generated/models/UserLastAction.ts
@@ -1,9 +1,11 @@
+import type { UserLastActionStatusEnum } from ".";
+
 /**
  * Details about a user's last action.
  * @category Models
  */
 export type UserLastAction = {
-  status: string;
+  status: UserLastActionStatusEnum;
   timestamp: number;
   relative: string;
 };
---
+++ src/generated/models/UserLastActionStatusEnum.ts
@@ -0,0 +1,2 @@
+/** @category Models */
+export type UserLastActionStatusEnum = "Online" | "Idle" | "Offline";
